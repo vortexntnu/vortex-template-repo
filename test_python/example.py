39a2f7131532501e1792c121f783ca529328518c
Black Robot <robot@example.com>
2023-03-05 16:07:45 +0000

    Committing black-format changes

--- test_python/example.py
+++ test_python/example.py
@@ -7,11 +7,11 @@ x = np.linspace(-10, 10, 1000)
 # Generate random polynomial coefficients
 coefficients = np.random.randn(5)
 # Compute the y values for the polynomial
-y=np.polyval(coefficients, x)
+y = np.polyval(coefficients, x)
 
 # Plot the polynomial
 plt.plot(x, y)
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Random Polynomial')
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title("Random Polynomial")
 plt.show()
